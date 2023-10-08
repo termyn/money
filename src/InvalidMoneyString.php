6016a70375c49ea92bf586bb2647b54e1baf4a2b
Edo Barnas <iam@edobarnas.com>
2023-10-08 15:42:31 +0200

    Update an error message

--- src/InvalidMoneyString.php
+++ src/InvalidMoneyString.php
@@ -11,7 +11,7 @@ final class InvalidMoneyString extends InvalidArgumentException
     public function __construct(string $money)
     {
         parent::__construct(
-            sprintf('Money string "%s" is invalid. Valid string should be: $1500.00 | -$1250.15 | +$800.15', $money)
+            sprintf('Money string "%s" is invalid. It should be valid e.g: $1500.00 | -$1250.15 | +$800.15', $money)
         );
     }
 }
